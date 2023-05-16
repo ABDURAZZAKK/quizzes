623a2bb6b52fee452b7745dc97b3402851cdaa28
abuad <abdurazzakk.m@gmail.com>
2023-05-16 17:48:55 +0300

    изменил формирование DATABASE_URL чтобы можно было ничего не меняя работать как полностью из докера так и используя только бд

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
+PG_HOST=os.getenv('POSTGRES_HOST')
+PG_PORT=os.getenv('POSTGRES_PORT')
+PG_USER=os.getenv('POSTGRES_USER')
+PG_PASSWORD=os.getenv('POSTGRES_PASSWORD')
+PG_DB=os.getenv('POSTGRES_DB')
 # URLs
-# "postgresql://root:root@localhost:5433/HEADMAN_DB"
-DATABASE_URL=os.getenv('PG_DATABASE_URL')
-
+# "postgresql+asyncpg://POSTGRES:POSTGRES@localhost:5433/quizzes"
+DATABASE_URL= f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
 
